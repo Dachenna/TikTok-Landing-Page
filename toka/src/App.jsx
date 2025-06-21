00e2c59e603eb36720bfe8d00bc5e980e8c83459
David <davidinprograming@gmail.com>
2025-06-21 20:47:45 +0100

    new file:   toka/.gitignore 	new file:   toka/README.md 	new file:   toka/eslint.config.js 	new file:   toka/index.html 	new file:   toka/package-lock.json 	new file:   toka/package.json 	new file:   toka/postcss.config.js 	new file:   toka/public/vite.svg 	new file:   toka/src/App.jsx 	new file:   toka/src/assets/react.svg 	new file:   toka/src/index.css 	new file:   toka/src/main.jsx 	new file:   toka/tailwind.config.js 	new file:   toka/vite.config.js

---
+++ toka/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='bg-gradient-to-b from-[#4C214B] to-[#201C2D] min-h-screen max-w-screen-lg '>
+      <h1 className='text-4xl'>Welcome to Toka</h1>
+    </div>
+  )
+}
+
+export default App
